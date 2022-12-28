01f6262824bc7cbc671a4ee388ae9b8466462d0e
pyrolass <las.suleman1@gmail.com>
2022-12-28 17:23:38 +0300

    before final push

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,2 +1,2 @@
-String chatToken = "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";
-String translationToken = "AIzaSyCTk71_rQe8vCqsoZmi_pKbIzMGAP-93UQ";
+String chatToken = "";
+String translationToken = "";
