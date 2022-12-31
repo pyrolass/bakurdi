1e80e114023f34f49bafce22aba1ca4e55403e64
pyrolass <las.suleman1@gmail.com>
2022-12-31 19:15:14 +0300

    ui changed

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('bakurdi x ChatpGPT'),
+        title: const Text('bakurdi'),
       ),
       body: GestureDetector(
         onTap: () {
